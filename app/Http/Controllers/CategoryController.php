8453a267320806fdb3db53c04d35eb8fda8b3818
Patipan Changlek <noajoo-8@hotmail.com>
2020-08-16 14:11:44 +0700

    update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function user(){
-
+    public function index()
+    {
+        $categories = Category::all();
+        return $categories;
     }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+        return $category;
+    }
+
 
     public function create(){
 
